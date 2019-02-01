1f00dc74c226754f9d7a0b6429e438e2086cbe23
SNikitinI <snikitin@akolchin.com>
2019-02-01 12:59:45 +0300

    Initial commit

---
+++ src/main/java/com/akolchin/client/gin/ClientModule.java
@@ -0,0 +1,25 @@
+package com.akolchin.client.gin;
+
+import com.akolchin.client.application.ApplicationModule;
+import com.akolchin.client.place.NameTokens;
+import com.akolchin.client.resources.ResourceLoader;
+import com.gwtplatform.mvp.client.annotations.DefaultPlace;
+import com.gwtplatform.mvp.client.annotations.ErrorPlace;
+import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.gwtplatform.mvp.client.gin.DefaultModule;
+
+public class ClientModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        install(new DefaultModule
+                .Builder()
+                .defaultPlace(NameTokens.HOME)
+                .errorPlace(NameTokens.HOME)
+                .unauthorizedPlace(NameTokens.HOME)
+                .build());
+        install(new ApplicationModule());
+
+        bind(ResourceLoader.class).asEagerSingleton();
+    }
+}
---
+++ src/main/java/com/akolchin/client/place/NameTokens.java
@@ -0,0 +1,5 @@
+package com.akolchin.client.place;
+
+public class NameTokens {
+    public static final String HOME = "/";
+}
